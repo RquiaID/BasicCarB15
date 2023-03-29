b0e5f55fcf56930938bc8a90e50c6428a80cff3e
RquiaID <idbella.rquia@gmail.com>
2023-03-29 19:17:29 -0400

    the basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      BasicsStructure audi=new BasicsStructure();
+      audi.color="black";
+      audi.model="A3";
+      audi.power="5000cc";
+    }
+}
