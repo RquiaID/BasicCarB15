c6e3deddd92f2aeeaf2237bf5fcb08f3770cb411
RquiaID <idbella.rquia@gmail.com>
2023-03-29 21:24:10 -0400

    the color feature has been added

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
       audi.color="black";
       audi.model="A3";
       audi.power="5000cc";
+
+      color col=new color();
+      col.colorLights="True";
+      col.colorType="false";
+      col.colorWindows="True";
+
     }
 }
